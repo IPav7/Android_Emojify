3e6fec540da4377f74152b70d8e334cce3a90550
IPav7 <pavinich1997@mail.ru>
2018-03-21 21:27:07 +0300

    Emoji replace face

--- app/src/main/java/com/example/android/emojify/Emojifier.java
+++ app/src/main/java/com/example/android/emojify/Emojifier.java
@@ -22,47 +22,94 @@ import com.google.android.gms.vision.face.Landmark;
 
 public class Emojifier {
 
-    static Paint mPaint;
-    static Canvas mCanvas;
+    private static final float EMOJI_SCALE_FACTOR = .9f;
     private static final double EYE_THRESHOLD = 0.5d;
     private static final double SMILE_THRESHOLD = 0.15d;
 
     private static final String LOG_TAG = Emojifier.class.getSimpleName();
 
-    public static void detectFacesAndOverlayEmoji(Context context, Bitmap bitmap){
+    public static Bitmap detectFacesAndOverlayEmoji(Context context, Bitmap bitmap){
         FaceDetector faceDetector = new FaceDetector.Builder(context)
                 .setTrackingEnabled(false)
                 .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
                 .build();
         Frame frame = new Frame.Builder().setBitmap(bitmap).build();
         SparseArray<Face> faces = faceDetector.detect(frame);
-       // Drawable emojiBitmap;
+        Bitmap emojiBitmap = null;
             for (int i = 0; i < faces.size(); i++) {
-                //      switch (
-                whichEmoji(context, faces.valueAt(i), i);
-            }
-               /*     case SMILE:
+                      switch (whichEmoji(faces.valueAt(i))){
+                    case SMILE:
+                        emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.smile);
                         break;
                     case FROWN:
+                        emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.frown);
                         break;
                     case LEFT_WINK:
+                        emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.leftwink);
                         break;
                     case RIGHT_WINK:
+                        emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.rightwink);
                         break;
                     case LEFT_WINK_FROWN:
+                        emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.leftwinkfrown);
                         break;
                     case CLOSED_EYE_FROWN:
+                        emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.closed_frown);
                         break;
                     case CLOSED_EYE_SMILE:
+                        emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.closed_smile);
                         break;
                     case RIGHT_WINK_FROWN:
+                        emojiBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.rightwinkfrown);
                         break;
                 }
-                }*/
+                bitmap = addBitmapToFace(bitmap, emojiBitmap, faces.valueAt(i));
+                }
         faceDetector.release();
+            return bitmap;
     }
 
-    private static void whichEmoji(Context context, Face face, int i){
+    /**
+     * Combines the original picture with the emoji bitmaps
+     *
+     * @param backgroundBitmap The original picture
+     * @param emojiBitmap      The chosen emoji
+     * @param face             The detected face
+     * @return The final bitmap, including the emojis over the faces
+     */
+    private static Bitmap addBitmapToFace(Bitmap backgroundBitmap, Bitmap emojiBitmap, Face face) {
+
+        // Initialize the results bitmap to be a mutable copy of the original image
+        Bitmap resultBitmap = Bitmap.createBitmap(backgroundBitmap.getWidth(),
+                backgroundBitmap.getHeight(), backgroundBitmap.getConfig());
+
+        // Scale the emoji so it looks better on the face
+        float scaleFactor = EMOJI_SCALE_FACTOR;
+
+        // Determine the size of the emoji to match the width of the face and preserve aspect ratio
+        int newEmojiWidth = (int) (face.getWidth() * scaleFactor);
+        int newEmojiHeight = (int) (emojiBitmap.getHeight() *
+                newEmojiWidth / emojiBitmap.getWidth() * scaleFactor);
+
+
+        // Scale the emoji
+        emojiBitmap = Bitmap.createScaledBitmap(emojiBitmap, newEmojiWidth, newEmojiHeight, false);
+
+        // Determine the emoji position so it best lines up with the face
+        float emojiPositionX =
+                (face.getPosition().x + face.getWidth() / 2) - emojiBitmap.getWidth() / 2;
+        float emojiPositionY =
+                (face.getPosition().y + face.getHeight() / 2) - emojiBitmap.getHeight() / 3;
+
+        // Create the canvas and draw the bitmaps to it
+        Canvas canvas = new Canvas(resultBitmap);
+        canvas.drawBitmap(backgroundBitmap, 0, 0, null);
+        canvas.drawBitmap(emojiBitmap, emojiPositionX, emojiPositionY, null);
+
+        return resultBitmap;
+    }
+
+    private static Emoji whichEmoji(Face face){
         Emoji emoji;
 
       /*  Toast.makeText(context,
@@ -94,11 +141,8 @@ public class Emojifier {
                 emoji = Emoji.LEFT_WINK_FROWN;
             else emoji = Emoji.CLOSED_EYE_FROWN;
         }
-        Toast.makeText(context, "Face num" + i + " Emoji is " + emoji.name(), Toast.LENGTH_SHORT).show();
-       // return emoji;
+        return emoji;
     }
-
-
 
     private enum Emoji {
         SMILE,
@@ -109,25 +153,6 @@ public class Emojifier {
         RIGHT_WINK_FROWN,
         CLOSED_EYE_SMILE,
         CLOSED_EYE_FROWN
-    }
-
-    private static Canvas getCanvas(Bitmap bitmap){
-        if(mCanvas != null){
-            mCanvas.setBitmap(bitmap);
-        }
-        else {
-            mCanvas = new Canvas(bitmap);
-        }
-        return mCanvas;
-    }
-
-    private static Paint getPaint(){
-        if(mPaint == null){
-            mPaint = new Paint();
-            mPaint.setColor(Color.CYAN);
-            mPaint.setStyle(Paint.Style.STROKE);
-        }
-        return mPaint;
     }
 
 }
